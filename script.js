0bae15a1d32cd35df4875d22a60a8cf426bf9aeb
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2023-12-17 22:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const readlineSync = require('readline-sync');
 
-function firstWord(str) {
-  if (str.trim() === '' || !str.includes(' ')) {
-    return str;
-  }
-  return str.trim().split(' ')[0];
-}
-
-// Get user input using readlineSync
-const userInput = readlineSync.question('Enter a string: ');
-
-// Call the firstWord function with user input
-const result = firstWord(userInput);
-
-// Display the result
-console.log('Result:', result);

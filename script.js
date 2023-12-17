287e361b9ba7ba7f386aee98227b33976d5eef42
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2023-12-17 22:44:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+  function firstWord(str) {
+  // Check if the input string is empty or does not contain any space
+  if (str === '' || !str.includes(' ')) {
+    return str;
+  }
+
+  // Extract the first word by finding the index of the first space
+  const firstSpaceIndex = str.indexOf(' ');
+  const firstWord = str.slice(0, firstSpaceIndex);
+
+  return firstWord;
+}
+
+// Examples
+console.log(firstWord('see and stop'));    // Output: 'see'
+console.log(firstWord(' Hello World!'));    // Output: 'Hello'
+console.log(firstWord('12345'));             // Output: '12345'
+console.log(firstWord(''));                  // Output: ''
